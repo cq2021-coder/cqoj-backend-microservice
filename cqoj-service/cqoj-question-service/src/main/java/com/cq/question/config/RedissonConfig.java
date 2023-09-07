a834e2139e029d364d576fc8f1a0a2d3a2297882
cq2021-coder <2972084238@qq.com>
2023-09-07 18:02:54 +0800

    add: 配置redisson密码&&springboot项目部署的maven配置

--- cqoj-service/cqoj-question-service/src/main/java/com/cq/question/config/RedissonConfig.java
+++ cqoj-service/cqoj-question-service/src/main/java/com/cq/question/config/RedissonConfig.java
@@ -1,6 +1,7 @@
 package com.cq.question.config;
 
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
@@ -25,12 +26,17 @@ public class RedissonConfig {
 
     private Integer database;
 
+    private String password;
+
     @Bean
     public RedissonClient redissonClient() {
         // 1. 创建配置
         Config config = new Config();
         String redisAddress = String.format("redis://%s:%s", host, port);
         config.useSingleServer().setAddress(redisAddress).setDatabase(database);
+        if (StringUtils.isNotBlank(password)) {
+            config.useSingleServer().setPassword(password);
+        }
         // 2. 创建实例
         return Redisson.create(config);
     }
