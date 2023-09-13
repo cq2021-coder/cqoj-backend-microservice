7b6bbb05e7ba120647cdece6daa8c69a1d2df95c
cq2021-coder <2972084238@qq.com>
2023-09-13 21:59:23 +0800

    add: 配置RabbitMQ为手动确认

--- cqoj-service/cqoj-judge-service/src/main/java/com/cq/judge/codesandbox/impl/RemoteCodeSandbox.java
+++ cqoj-service/cqoj-judge-service/src/main/java/com/cq/judge/codesandbox/impl/RemoteCodeSandbox.java
@@ -2,10 +2,13 @@ package com.cq.judge.codesandbox.impl;
 
 import cn.hutool.http.HttpUtil;
 import cn.hutool.json.JSONUtil;
+import com.cq.common.exception.BusinessException;
+import com.cq.common.response.ResultCodeEnum;
 import com.cq.judge.codesandbox.CodeSandbox;
 import com.cq.model.dto.codesandbox.ExecuteCodeRequest;
 import com.cq.model.dto.codesandbox.ExecuteCodeResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * 远程代码沙箱
@@ -22,6 +25,9 @@ public class RemoteCodeSandbox implements CodeSandbox {
     @Override
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
         String responseStr = HttpUtil.post(URL, JSONUtil.toJsonStr(executeCodeRequest));
+        if (StringUtils.isBlank(responseStr)) {
+            throw new BusinessException(ResultCodeEnum.SYSTEM_ERROR, "远程代码沙箱无法访问");
+        }
         return JSONUtil.toBean(responseStr, ExecuteCodeResponse.class);
     }
 }
